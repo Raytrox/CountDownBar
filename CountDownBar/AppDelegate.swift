e76531681cf2c59936c36b05e4dd4d9446b969a5
Rui Yin <yinsharp25@gmail.com>
2019-08-05 20:14:08 -0700

    Initial Commit

---
+++ CountDownBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CountDownBar
+//
+//  Created by Rui Yin on 2019-08-05.
+//  Copyright © 2019 Rui Yin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CountDownBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CountDownBar
+//
+//  Created by Rui Yin on 2019-08-05.
+//  Copyright © 2019 Rui Yin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
